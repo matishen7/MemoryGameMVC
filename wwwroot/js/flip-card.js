90cd408492f1bfd892f8dd9a803fafe3754360ad
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-07-07 12:10:33 -0500

    fix flip card js

--- wwwroot/js/flip-card.js
+++ wwwroot/js/flip-card.js
@@ -1,43 +1,45 @@
-﻿//$(document).ready(function () {
-//    var openedCard = null;
-//    var disabledCards = [];
+﻿$(document).ready(function () {
+    var openedCard = null;
+    var disabledCards = [];
 
-//    // Handle click event on card cells
-//    $('#table_board td').click(function () {
-//        var clickedCard = $(this);
+    // Handle click event on card cells
+    $('#table_board td').click(function () {
+        var clickedCard = $(this);
+        if (clickedCard.hasClass('matched disabled')) {
+            event.preventDefault(); // Prevent the click event from being triggered on disabled cards
+            openedCard = null;
+            return; // Ignore the click event for disabled cards
+        }
 
-//        // Check if the clicked card is disabled (already matched)
-//        if (clickedCard.hasClass('matched')) {
-//            return; // Ignore the click event for disabled cards
-//        }
+        // Open the clicked card
+        clickedCard.find('.card-front').show();
+        clickedCard.find('.card-back').hide();
+        clickedCard.addClass('opened');
 
-//        // Open the clicked card
-//        clickedCard.find('.card-front').show();
-//        clickedCard.find('.card-back').hide();
-//        clickedCard.addClass('opened');
-
-//        // Check if this is the first opened card
-//        if (openedCard === null) {
-//            openedCard = clickedCard;
-//        } else {
-//            // Check if the images of the two cards match
-//            if (clickedCard.data('image') === openedCard.data('image')) {
-//                // Disable the matched cards
-//                disabledCards.push(clickedCard, openedCard);
-//                clickedCard.addClass('matched');
-//                openedCard.addClass('matched');
-//            } else {
-//                // If the images don't match, close both cards after a short delay
-//                setTimeout(function () {
-//                    clickedCard.find('.card-front').hide();
-//                    clickedCard.find('.card-back').show();
-//                    clickedCard.removeClass('opened');
-//                    openedCard.find('.card-front').hide();
-//                    openedCard.find('.card-back').show();
-//                    openedCard.removeClass('opened');
-//                    openedCard = null; // Reset the openedCard variable
-//                }, 3000); // Delay of 3 seconds (3000 milliseconds)
-//            }
-//        }
-//    });
-//});
+        // Check if this is the first opened card
+        if (openedCard === null) {
+            openedCard = clickedCard;
+        } else {
+            // Check if the images of the two cards match
+            if (clickedCard.data('image') === openedCard.data('image')) {
+                // Disable the matched cards
+                clickedCard.addClass('matched disabled');
+                openedCard.addClass('matched disabled');
+                disabledCards.push(clickedCard, openedCard);
+                openedCard = null;
+            } else {
+                // If the images don't match, close both cards after a short delay
+                var previouslyOpenedCard = openedCard; // Store reference to the previously opened card
+                openedCard = null; // Reset the openedCard variable
+                setTimeout(function () {
+                    clickedCard.find('.card-front').hide();
+                    clickedCard.find('.card-back').show();
+                    clickedCard.removeClass('opened');
+                    previouslyOpenedCard.find('.card-front').hide();
+                    previouslyOpenedCard.find('.card-back').show();
+                    previouslyOpenedCard.removeClass('opened');
+                }, 3000); // Delay of 3 seconds (3000 milliseconds)
+            }
+        }
+    });
+});
