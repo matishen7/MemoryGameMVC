6cee3a4c6dfa9e0fcdd0b7f59128b75a7105a3d8
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-07-21 12:34:06 -0500

    update index

--- Controllers/BoardController.cs
+++ Controllers/BoardController.cs
@@ -11,8 +11,8 @@ namespace MemoryGameMVC.Controllers
         private readonly ILogger<BoardController> _logger;
         private readonly IWebHostEnvironment _webHostEnvironment;
         private Board gameBoard;
-        private static Dictionary<string, Board> _gameBoards = new Dictionary<string, Board>();
-
+        private static Dictionary<string, CurrentGame> _progress = new Dictionary<string, CurrentGame>();
+        private string sessionId;
 
         public BoardController(ILogger<BoardController> logger, IWebHostEnvironment webHostEnvironment)
         {
@@ -21,35 +21,49 @@ namespace MemoryGameMVC.Controllers
 
         }
 
-        public IActionResult Index(int m, int n)
+        public IActionResult Index()
         {
-            string sessionId = HttpContext.Session.Id;
-            if (!_gameBoards.ContainsKey(sessionId))
+            if (_progress.Count == 0 || _progress.Values.First().EndGame)
             {
+                int m = 2;
+                int n = 2;
                 gameBoard = new Board(_webHostEnvironment, m, n);
                 gameBoard.Shuffle();
-                _gameBoards.Add(sessionId, gameBoard);
+                sessionId = HttpContext.Session.Id;
+                _progress.Add(sessionId, new CurrentGame()
+                {
+                    EndGame = false,
+                    GameBoard = gameBoard,
+                    GameLevel = 1,
+                });
             }
-
-            var currentGameBoard = _gameBoards[sessionId];
+            var currentProgress = _progress.Values.First();
             var viewModel = new BoardViewModel
             {
-                GameBoard = currentGameBoard,
-                MatchedCellsCount = currentGameBoard.matchedCells
+                GameBoard = currentProgress.GameBoard,
+                MatchedCellsCount = currentProgress.GameBoard.matchedCells
             };
+
+            if (viewModel.MatchedCellsCount == viewModel.GameBoard.GetM() * viewModel.GameBoard.GetN())
+            {
+                currentProgress.GameLevel++;
+                currentProgress.EndGame = true;
+                _progress.Clear();
+                _progress.Add(sessionId, currentProgress);
+            }
             return View("Board", viewModel);
         }
 
         public IActionResult FlipCard(int x, int y)
         {
-            var currentGameBoard = _gameBoards.Values.First();
-            var key = _gameBoards.Keys.First();
-            currentGameBoard.FlipCard(x, y);
-            _gameBoards[key] = currentGameBoard;
+            var currentProgress = _progress.Values.First();
+            var key = _progress.Keys.First();
+            currentProgress.GameBoard.FlipCard(x, y);
+            _progress[key] = currentProgress;
             var viewModel = new BoardViewModel
             {
-                GameBoard = currentGameBoard,
-                MatchedCellsCount = currentGameBoard.matchedCells
+                GameBoard = currentProgress.GameBoard,
+                MatchedCellsCount = currentProgress.GameBoard.matchedCells
             };
             return View("Board", viewModel);
         }
---
+++ Models/CurrentGame.cs
@@ -0,0 +1,11 @@
+﻿using MemoryGame;
+
+namespace MemoryGameMVC.Models
+{
+    public class CurrentGame
+    {
+        public bool EndGame;
+        public int GameLevel = 1;
+        public Board GameBoard;
+    }
+}
