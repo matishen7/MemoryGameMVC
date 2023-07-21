0f64d3872c11cb884de83ab90381122496650ef6
Muratbek Ishenbaev <ishenbaev7@gmail.com>
2023-07-21 13:57:53 -0500

    fix baord

--- Controllers/BoardController.cs
+++ Controllers/BoardController.cs
@@ -11,7 +11,7 @@ namespace MemoryGameMVC.Controllers
         private readonly ILogger<BoardController> _logger;
         private readonly IWebHostEnvironment _webHostEnvironment;
         private Board gameBoard;
-        private static Dictionary<string, CurrentGame> _progress = new Dictionary<string, CurrentGame>();
+        private static Dictionary<string, Board>? _progress = new Dictionary<string, Board>();
         private string sessionId;
 
         public BoardController(ILogger<BoardController> logger, IWebHostEnvironment webHostEnvironment)
@@ -23,47 +23,34 @@ namespace MemoryGameMVC.Controllers
 
         public IActionResult Index()
         {
-            if (_progress.Count == 0 || _progress.Values.First().EndGame)
+            int m = 2;
+            int n = 2;
+            if (_progress.Count == 0)
             {
-                int m = 2;
-                int n = 2;
                 gameBoard = new Board(_webHostEnvironment, m, n);
                 gameBoard.Shuffle();
                 sessionId = HttpContext.Session.Id;
-                _progress.Add(sessionId, new CurrentGame()
-                {
-                    EndGame = false,
-                    GameBoard = gameBoard,
-                    GameLevel = 1,
-                });
+                _progress.Add(sessionId, gameBoard);
             }
-            var currentProgress = _progress.Values.First();
+            var currentGameBoard = _progress.Values.First();
             var viewModel = new BoardViewModel
             {
-                GameBoard = currentProgress.GameBoard,
-                MatchedCellsCount = currentProgress.GameBoard.matchedCells
+                GameBoard = currentGameBoard,
+                MatchedCellsCount = currentGameBoard.matchedCells
             };
-
-            if (viewModel.MatchedCellsCount == viewModel.GameBoard.GetM() * viewModel.GameBoard.GetN())
-            {
-                currentProgress.GameLevel++;
-                currentProgress.EndGame = true;
-                _progress.Clear();
-                _progress.Add(sessionId, currentProgress);
-            }
             return View("Board", viewModel);
         }
 
         public IActionResult FlipCard(int x, int y)
         {
-            var currentProgress = _progress.Values.First();
+            var currentGameBoard = _progress.Values.First();
             var key = _progress.Keys.First();
-            currentProgress.GameBoard.FlipCard(x, y);
-            _progress[key] = currentProgress;
+            currentGameBoard.FlipCard(x, y);
+            _progress[key] = currentGameBoard;
             var viewModel = new BoardViewModel
             {
-                GameBoard = currentProgress.GameBoard,
-                MatchedCellsCount = currentProgress.GameBoard.matchedCells
+                GameBoard = currentGameBoard,
+                MatchedCellsCount = currentGameBoard.matchedCells
             };
             return View("Board", viewModel);
         }
